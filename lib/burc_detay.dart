a6ea398a8cd35fd58968400545eb766d6916b559
leventuysal <leventuysal@gmail.com>
2020-05-31 21:24:22 +0300

    initial commit

--- lib/burc_detay.dart
+++ lib/burc_detay.dart
@@ -1,8 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutterburcrehberi/burc_liste.dart';
 
-class BurcDetay extends StatelessWidget {
+import 'models/burc.dart';
+import 'package:palette_generator/palette_generator.dart';
+
+class BurcDetay extends StatefulWidget {
+  int gelenIndex;
+
+  BurcDetay(this.gelenIndex);
+
+  @override
+  _BurcDetayState createState() => _BurcDetayState();
+}
+
+class _BurcDetayState extends State<BurcDetay> {
+  Burc secilenBurc;
+  Color baskinRenk;
+  Color baskinAltRenk;
+  PaletteGenerator paletteGenerator;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    secilenBurc = BurcListesi.tumBurclar[widget.gelenIndex];
+    baskinRengiBul();
+  }
+
+  void baskinRengiBul() {
+    Future<PaletteGenerator> futurePaletteGenerator =
+        PaletteGenerator.fromImageProvider(
+            AssetImage("images/" + secilenBurc.burcBuyukResim));
+    futurePaletteGenerator.then((value) {
+      paletteGenerator = value;
+
+      //debugPrint("domainan renk:" + paletteGenerator.dominantColor.toString());
+      setState(() {
+        baskinRenk = paletteGenerator.dominantColor.color;
+        baskinAltRenk = paletteGenerator.lightMutedColor.color;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Scaffold(
+      body: CustomScrollView(
+        primary: true,
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 250,
+            pinned: true,
+            primary: true,
+            backgroundColor: baskinRenk != null ? baskinRenk : Colors.pink,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(secilenBurc.burcAdi + " Burcu Özellikleri"),
+              centerTitle: true,
+              background: Image.asset(
+                "images/${secilenBurc.burcBuyukResim}",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              margin: EdgeInsets.all(8),
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: baskinAltRenk != null ? baskinAltRenk : Colors.pink.shade50,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              //color: Colors.pink.shade100,
+              child: SingleChildScrollView(
+                child: Text(
+                  secilenBurc.burcDetayi,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutterburcrehberi/burc_detay.dart';
 import 'package:flutterburcrehberi/burc_liste.dart';
+import 'palette_generator.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp2());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Burç Rehberi",
       debugShowCheckedModeBanner: false,
+      initialRoute: "/burclistesi",
+      routes: {
+        "/": (context) => BurcListesi(),
+        "/burclistesi": (context) => BurcListesi(),
+      },
+      onGenerateRoute: (RouteSettings settings) {
+        List<String> pathElemanlari = settings.name.split("/");
+        if (pathElemanlari[1] == "burcdetay")
+          {
+            return MaterialPageRoute(builder: (context)=> BurcDetay(int.parse(pathElemanlari[2])));
+          }
+        return null;
+      },
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: BurcListesi(),
+      //home: BurcListesi(),
     );
   }
 }
