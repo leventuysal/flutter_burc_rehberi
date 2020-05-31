6c29dd864f4af22ad750e9e2577aa1a578ef143b
leventuysal <leventuysal@gmail.com>
2020-05-31 18:12:30 +0300

    initial commit

--- lib/burc_liste.dart
+++ lib/burc_liste.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class BurcListesi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Burç Rehberi",
+        ),
+      ),
+      body: Container(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Burç Rehberi",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
